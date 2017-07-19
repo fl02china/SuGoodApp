d97affe188f650234d030f9176c4307183dcf392
fl02china <fl02china@126.com>
2017-07-19 15:59:46 +0800

    bug修复 Signed-off-by: fl02china <fl02china@126.com>

--- GoApplication/SuGoodApp/src/com/sugood/app/activity/TakeawaySubmitOrderActivity.java
+++ GoApplication/SuGoodApp/src/com/sugood/app/activity/TakeawaySubmitOrderActivity.java
@@ -118,53 +118,8 @@ public class TakeawaySubmitOrderActivity extends BaseActivity {
     private final IWXAPI msgApi = WXAPIFactory.createWXAPI(this, null);
     private FinshReceiver mFinsh;
     List<ShopCarProduct> shopCarList = new ArrayList<>();
-    // Realm realm = Realm.getDefaultInstance();
-    @SuppressLint("HandlerLeak")
-//    private Handler mHandler = new Handler() {
-//        @SuppressWarnings("unused")
-//        public void handleMessage(Message msg) {
-//            switch (msg.what) {
-//
-//                case SDK_PAY_FLAG: {
-//                    @SuppressWarnings("unchecked")
-//                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-//
-//                    /**
-//                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-//                     */
-//                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
-//                    Log.e(TAG, "resultInfo1111: "+resultInfo);
-//                    String resultStatus = payResult.getResultStatus();
-//                    // 判断resultStatus 为9000则代表支付成功
-//                    if (TextUtils.equals(resultStatus, "9000")) {
-//                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-//                        Toast.makeText(TakeawaySubmitOrderActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
-//                        Intent intent = new Intent("FinishActivity");
-//                        sendBroadcast(intent);//发送对应的广播
-//
-//                        finish();
-///*
-//                        RealmResults<ShopCarList> list = realm.where(ShopCarList.class).findAll();
-//                        list.addChangeListener(new RealmChangeListener<RealmResults<ShopCarList>>() {
-//                            @Override
-//                            public void onChange(RealmResults<ShopCarList> element) {
-//                                element.deleteAllFromRealm();
-//                            }
-//                        });*/
-//
-//
-//                    } else {
-//                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-//                        Toast.makeText(TakeawaySubmitOrderActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
-//                    }
-//                    break;
-//                }
-//
-//                default:
-//                    break;
-//            }
-//        }
-//    };
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -495,6 +450,7 @@ public class TakeawaySubmitOrderActivity extends BaseActivity {
 
                                     body.put("type",json.getString("type"));
                                     body.put("orderId",orderid);
+                                    body.put("shopName", shop.getShopName());
                                     Intent intent = new Intent();
                                     intent.putExtra("shopname", shop.getShopName());
                                     intent.putExtra("orderId", orderid);
